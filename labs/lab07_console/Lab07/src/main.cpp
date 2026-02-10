5f8f2f1f64d25160d6d0738c6da7e586b9ca11cd
Artem Kodzhebash <artem.kodzhebash.03@gmail.com>
2026-02-11 02:08:59 +0700

    Решение заданий 1 и 2

--- labs/lab07_console/Lab07/src/main.cpp
+++ labs/lab07_console/Lab07/src/main.cpp
@@ -1,31 +1,43 @@
 #include <iostream>
+#include <math.h>
+#include <stdlib.h>
+using namespace std;
 
-// Lab 07
-// TODO: реализуйте решение по заданию в labs/lab07_console/README.md
-//
-// Рекомендация по выводу:
-// - без лишнего текста
-// - числа через пробел
-// - если несколько строк — в фиксированном порядке
-int main() {
-    // Задание 1: вычисление значений функции.
-    // Ввод: x0 — начальная точка интервала.
-    double x0 = 0.0;
-    if (!(std::cin >> x0)) {
-        return 0;
-    }
-    // dfdfdf
-    // // dfd/df
-    // /dfdfd
-    // TODO: вычислите и выведите 10 значений функции на (0, 4].
+int main()
+{
+	double x = 0.0;
+    double end = 4.0;
+	double step = end / 10.0; // шаг 4/10 = 0.4
+	for (int i = 0; i < 10; i = i + 1)
+	{
+		x = x + step;
+		double y = sin(x) / x;
+		cout  << y << endl;
+	}
 
-    // Задание 2: сумма чисел Фибоначчи.
-    // Ввод: MAX — ограничение на сумму.
-    long long max_value = 0;
-    if (!(std::cin >> max_value)) {
-        return 0;
-    }
-
-    // TODO: вычислите N и S (S <= MAX) и выведите результаты.
-    return 0;
+	return 0;
 }
+
+//int main()
+//{
+//	double max_value = 100.0;
+//	double f0 = 0.0;
+//	double f1 = 1.0;
+//	double sum = 1.0;
+//	int n = 2;
+//
+//	while (sum < max_value)
+//	{
+//		double next = f0 + f1;
+//		sum = sum + next;
+//		n = n + 1;
+//		f0 = f1;
+//		f1 = next;
+//	}
+//
+//	cout << n << " " << sum;
+//	return  0;
+//}
+
+
+	
